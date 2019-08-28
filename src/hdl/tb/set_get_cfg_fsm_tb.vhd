12802fc4c6b70e8d5739722787b4e3c230f419fb
Lucas Parker <lpp@lanl.gov>
2019-08-28 17:04:36 -0600

    Adding `trigger_out` to fsm
    Also fixing TB's. This branch should have been named for the
    trigger_out, but I forgot that it was added (and setted off the tb
    updates).

--- src/hdl/tb/set_get_cfg_fsm_tb.vhd
+++ src/hdl/tb/set_get_cfg_fsm_tb.vhd
@@ -5,24 +5,28 @@ use ieee.STD_LOGIC_UNSIGNED.all;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
 
-entity vata_fsm_v3_tb is
-end vata_fsm_v3_tb;
+entity set_get_cfg_fsm_tb is
+end set_get_cfg_fsm_tb;
 
-architecture TB_ARCH of vata_fsm_v3_tb is
+architecture TB_ARCH of set_get_cfg_fsm_tb is
       -- Component declaration of the tested unit
     component vata_460p3_iface_fsm
         port (
             clk_100MHz         : in std_logic; -- 100 ns
             rst_n              : in std_logic;
-            trigger_get_config : in std_logic;
-            trigger_set_config : in std_logic;
+            trigger_in         : in std_logic;
+            trigger_out        : out std_logic;
+            get_config         : in std_logic;
+            set_config         : in std_logic;
+            cp_data_done       : in std_logic;
+            hold_time          : in std_logic_vector(15 downto 0);
             vata_s0            : out std_logic;
             vata_s1            : out std_logic;
             vata_s2            : out std_logic;
             vata_s_latch       : out std_logic;
-            vata_i1_out        : out std_logic;
-            vata_i3_out        : out std_logic;
-            vata_i4_out        : out std_logic;
+            vata_i1            : out std_logic;
+            vata_i3            : out std_logic;
+            vata_i4            : out std_logic;
             vata_o5            : in std_logic;
             vata_o6            : in std_logic;
             bram_addr          : out std_logic_vector(31 downto 0);
@@ -30,8 +34,8 @@ architecture TB_ARCH of vata_fsm_v3_tb is
             bram_wea           : out std_logic_vector (3 downto 0) := (others => '0');
             cfg_reg_from_ps    : in std_logic_vector(519 downto 0);
             state_counter_out  : out std_logic_vector(15 downto 0);
-            cfg_reg_indx_out   : out std_logic_vector(9 downto 0);
-            cfg_reg_from_vata_out : out std_logic_vector(7 downto 0);
+            reg_indx_out       : out std_logic_vector(9 downto 0);
+            reg_from_vata_out  : out std_logic_vector(378 downto 0);
             state_out          : out std_logic_vector(7 downto 0));
     end component;
 
@@ -48,8 +52,8 @@ architecture TB_ARCH of vata_fsm_v3_tb is
     
     signal clk : std_logic;
     signal rst_n : std_logic;
-    signal trigger_get_config : std_logic := '0';
-    signal trigger_set_config : std_logic := '0';
+    signal get_config : std_logic := '0';
+    signal set_config : std_logic := '0';
     signal vata_s0 : std_logic;
     signal vata_s1 : std_logic;
     signal vata_s2 : std_logic;
@@ -65,8 +69,8 @@ architecture TB_ARCH of vata_fsm_v3_tb is
     signal bram_wea    : std_logic_vector (3 downto 0) := (others => '0');
 
     signal state_counter_out : std_logic_vector(15 downto 0);
-    signal cfg_reg_indx_out : std_logic_vector(9 downto 0);
-    signal cfg_reg_from_vata_out : std_logic_vector(7 downto 0);
+    signal reg_indx_out : std_logic_vector(9 downto 0);
+    signal reg_from_vata_out : std_logic_vector(378 downto 0);
     signal state_out : std_logic_vector(7 downto 0);
 
     signal sim_o5 : std_logic := '0';
@@ -97,15 +101,15 @@ begin
 
     trigger_proc : process
     begin
-        trigger_set_config <= '0';
-        trigger_get_config <= '0';
+        set_config <= '0';
+        get_config <= '0';
         sim_o5 <= '0';
         wait for 5 * Tpd;
-        trigger_set_config <= '1';
+        set_config <= '1';
         wait for Tpd;
-        trigger_set_config <= '0';
+        set_config <= '0';
         wait for 11 ms; -- Roughly how long it takes to set config
-        trigger_get_config <= '1';
+        get_config <= '1';
         sim_o5 <= '1';
         -- Set a pattern that should be easy to recognize
         -- Starts/stops with '1', so if all bits accounted for we write/read the correct number of bits
@@ -113,7 +117,7 @@ begin
         cfg_reg(31 downto 0) <= x"00000015"; -- "0..010101"
         cfg_reg(519 downto 512) <= x"AA";    -- "10101010"
         wait for Tpd;
-        trigger_get_config <= '0';
+        get_config <= '0';
         wait;
     end process;
 
@@ -141,26 +145,30 @@ begin
 
     UUT : vata_460p3_iface_fsm
         port map (
-            clk_100MHz => clk,
-            rst_n => rst_n,
-            trigger_set_config => trigger_set_config,
-            trigger_get_config => trigger_get_config,
-            vata_s0 => vata_s0,
-            vata_s1 => vata_s1,
-            vata_s2 => vata_s2,
-            vata_s_latch => vata_s_latch,
-            vata_i1_out => vata_i1,
-            vata_i3_out => vata_i3,
-            vata_i4_out => vata_i4,
-            vata_o5 => vata_o5,
-            vata_o6 => vata_o6,
-            bram_addr => bram_addr,
-            bram_dwrite => bram_dwrite,
-            bram_wea => bram_wea,
-            cfg_reg_from_ps => cfg_reg,
+            clk_100MHz        => clk,
+            rst_n             => rst_n,
+            trigger_in        => '0',
+            trigger_out       => open,
+            set_config        => set_config,
+            get_config        => get_config,
+            cp_data_done      => '0',
+            hold_time         => x"0000",
+            vata_s0           => vata_s0,
+            vata_s1           => vata_s1,
+            vata_s2           => vata_s2,
+            vata_s_latch      => vata_s_latch,
+            vata_i1           => vata_i1,
+            vata_i3           => vata_i3,
+            vata_i4           => vata_i4,
+            vata_o5           => vata_o5,
+            vata_o6           => vata_o6,
+            bram_addr         => bram_addr,
+            bram_dwrite       => bram_dwrite,
+            bram_wea          => bram_wea,
+            cfg_reg_from_ps   => cfg_reg,
             state_counter_out => state_counter_out,
-            cfg_reg_indx_out => cfg_reg_indx_out,
-            cfg_reg_from_vata_out => cfg_reg_from_vata_out,
-            state_out => state_out);
+            reg_indx_out      => reg_indx_out,
+            reg_from_vata_out => reg_from_vata_out,
+            state_out         => state_out);
             
 end TB_ARCH;
