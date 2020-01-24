a6c381f68b6a1ca0820be18a157555d0b6a9cdcb
Lucas Parker <thelucasparker@gmail.com>
2020-01-24 12:02:22 -0700

    Remove calibrator functions from vata control
    Also, now bunding `vata_ctrl` and `cal_ctrl` into
    single library, which should be linked against by code built
    on the si-layer processor.

--- src/petalinux/si-layer/project-spec/meta-user/recipes-apps/vatactrl/files/vatactrl/include/vata_constants.hpp
+++ src/petalinux/si-layer/project-spec/meta-user/recipes-apps/vatactrl/files/vatactrl/include/vata_constants.hpp
@@ -15,25 +15,20 @@
 #define DATA_PACKET_HEADER_NBYTES       3   //  [N-DATA_TOT, TIME0, TIME1]
 
 #define CFG_REG_OFFSET                  1
-#define READ_CFG_REG_OFFSET             32
+#define READ_CFG_REG_OFFSET             31
 #define HOLD_TIME_REG_OFFSET            18
-#define CAL_DAC_REG_OFFSET              19
-#define POWER_CYCLE_REG_OFFSET          20
-#define TRIGGER_ACK_TIMEOUT_REG_OFFSET  21
-#define RUNNING_TIMER_OFFSET            49
-#define LIVE_TIMER_OFFSET               51
-#define EVENT_COUNT_OFFSET              53
+#define POWER_CYCLE_REG_OFFSET          19
+#define TRIGGER_ACK_TIMEOUT_REG_OFFSET  20
+#define RUNNING_TIMER_OFFSET            48
+#define LIVE_TIMER_OFFSET               50
+#define EVENT_COUNT_OFFSET              52
 
 #define AXI0_CTRL_SET_CONF              0
 #define AXI0_CTRL_GET_CONF              1
-#define AXI0_CTRL_SET_CAL_DAC           2
-#define AXI0_CTRL_TRIGGER_EXT_CAL       3
-#define AXI0_CTRL_TRIGGER_INT_CAL       4
-#define AXI0_CTRL_POWER_CYCLE           5
-#define AXI0_CTRL_RST_COUNTERS          6
-#define AXI0_CTRL_RST_EV_COUNT          7
-
-#define MAX_CAL_DAC_VAL                 4095
+#define AXI0_CTRL_TRIGGER_INT_CAL       2
+#define AXI0_CTRL_POWER_CYCLE           3
+#define AXI0_CTRL_RST_COUNTERS          4
+#define AXI0_CTRL_RST_EV_COUNT          5
 
 typedef uint8_t u8;
 typedef uint16_t u16;
