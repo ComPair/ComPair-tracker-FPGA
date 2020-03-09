5269aaefa742cef04e5e79f4dab8703f01610667
Lucas Parker <thelucasparker@gmail.com>
2020-03-09 17:08:26 -0600

    Reorganizing code that runs on the zynq
    * Bringing together sources for sictrl library, putting in one location
    * Moving sources for vatactrl, calctrl, dacctrl executables into one
    location

---
+++ src/petalinux/apps/silayer-ctrl/src/vatactrl.cpp
@@ -0,0 +1,202 @@
+/* vatactrl
+ * ========
+ * 
+ * Program for performing basic interactions with a single vata asic.
+ *
+ * Return codes:
+ * -------------
+ *      * 0: all good
+ *      * 1: error parsing command line args
+ *      * 2: invalid vata number provided
+ *      * 3: error setting asic configuration.
+ *      * 4: invalid cal-dac value.
+ */
+
+#include <cassert>
+#include <cstring>
+
+#include "vata_ctrl.hpp"
+
+#define VT_PARSE_ARGS_ERR      1
+#define VT_VATA_NUM_ERR        2
+#define VT_SET_CONFIG_ERR      3
+#define VT_CAL_DAC_ERR         4
+#define VT_TRIGGER_ENA_BIT_ERR 5
+
+void usage(char *argv0) {
+    std::cout << "Usage: " << argv0 << " ASIC-NUM [OPTIONS]" << std::endl
+              << "  ASIC-NUM : Number of the ASIC we are targeting" << std::endl
+              << "  OPTIONS:" << std::endl
+              << "    --set-config FNAME        : set configuration from file FNAME" << std::endl
+              << "    --get-config FNAME        : get configuration, write to file FNAME" << std::endl
+              << "    --set-hold HOLD           : set the ASIC hold time to HOLD clk-cycles" << std::endl
+              << "    --get-hold                : write ASIC hold time to stdout" << std::endl
+              << "    --get-counters            : print 'running' and 'live' counters to stdout" << std::endl
+              << "    --reset-counters          : reset the 'running' and 'live' counters" << std::endl
+              << "    --trigger-enable ENA-BIT  : enable triggering from source associated with ENA-BIT" << std::endl
+              << "    --trigger-disable ENA-BIT : disable triggering from source associated with ENA-BIT" << std::endl
+              << "    --get-trigger-ena-mask    : print the trigger-enable mask to stdout" <<std::endl
+              << "    --force-trigger           : force ASIC to perform data readout" << std::endl
+              << "    --set-ack-timeout TIMEOUT : set the trigger ack timeout to TIMEOUT" << std::endl
+              << "    --get-ack-timeout         : print the current trigger ack timeout to stdout" << std::endl
+              << "    --get-event-count         : print event counter to stdout" << std::endl
+              << "    --reset-event-count       : reset the event counter" << std::endl
+              << "    --get-n-fifo              : print number of data packets in fifo to stdout" << std::endl
+              << "    --single-read-fifo        : read a single data packet, print to stdout" << std::endl
+              << "    --read-fifo               : read the entire fifo, each packet to a single line of stdout" << std::endl;
+}
+
+VataCtrl get_vata_from_args(int argc, char **argv) {
+    assert (argc > 1);    
+    char *endptr;
+    int nvata = strtol(argv[1], &endptr, 0);
+    if (*endptr != '\0') {
+        // Could not parse arg... throw error
+        std::cerr << "ERROR: first argument (" << argv[1] << ") is not a number." << std::endl;
+        throw 1;
+    }
+    if (nvata < 0 || nvata >= (int)N_VATA) {
+        std::cerr << "ERROR: Provided vata number (" << argv[1] << ") is not valid." << std::endl;
+        std::cerr << "       Vata number should be in [0, " << N_VATA << ")." << std::endl;
+        throw 2;
+    }
+    return VataCtrl(nvata);
+}
+
+int parse_args(VataCtrl vata, int argc, char **argv) {
+    for (int i=0; i<argc; i++) {
+        if (strcmp("--help", argv[i]) == 0 || strcmp("-h", argv[i]) == 0) {
+            // Exit and print usage...
+            return VT_PARSE_ARGS_ERR;
+        } else if (strcmp("--set-config", argv[i]) == 0) { 
+            if (++i >= argc) {
+                std::cerr << "ERROR: No configuration source file given." << std::endl;
+                return VT_PARSE_ARGS_ERR;
+            }
+            if (!vata.set_check_config(argv[i])) {
+                std::cerr << "ERROR: Configuration file setting unsuccessful." << std::endl;
+                return VT_SET_CONFIG_ERR;
+            }
+        } else if (strcmp("--get-config", argv[i]) == 0) { 
+            if (++i >= argc) {
+                std::cerr << "ERROR: No configuration destination file given." << std::endl;
+                return VT_PARSE_ARGS_ERR;
+            }
+            vata.get_config(argv[i]); 
+        } else if (strcmp("--set-hold", argv[i]) == 0) { 
+            if (++i >= argc) {
+                std::cerr << "ERROR: No hold time specified." << std::endl;
+                return VT_PARSE_ARGS_ERR;
+            }
+            u32 hold_time = (u32)atoi(argv[i]);
+            vata.set_hold_delay(hold_time);
+        } else if (strcmp("--get-hold", argv[i]) == 0) { 
+            int hold_time = (int)vata.get_hold_delay();
+            std::cout << hold_time << std::endl;
+        } else if (strcmp("--get-counters", argv[i]) == 0) { 
+            u64 running, live;
+            vata.get_counters(running, live);
+            std::cout << running << " " << live << std::endl;
+        } else if (strcmp("--reset-counters", argv[i]) == 0) { 
+            vata.reset_counters();
+        } else if (strcmp("--trigger-enable", argv[i]) == 0) { 
+            if (++i >= argc) {
+                std::cerr << "ERROR: No trigger enable bit specified." << std::endl;
+                return VT_PARSE_ARGS_ERR;
+            }
+            if (strcmp("all", argv[i]) == 0) {
+                vata.trigger_enable_all();
+            } else if (vata.trigger_enable(atoi(argv[i])) != 0) {
+                std::cerr << "ERROR: Bad trigger enable bit specified." << std::endl;
+                return VT_TRIGGER_ENA_BIT_ERR;
+            }
+        } else if (strcmp("--trigger-disable", argv[i]) == 0) { 
+            if (++i >= argc) {
+                std::cerr << "ERROR: No trigger enable bit specified." << std::endl;
+                return VT_PARSE_ARGS_ERR;
+            }
+            if (strcmp("all", argv[i]) == 0) {
+                vata.trigger_disable_all();
+            } else if (vata.trigger_disable(atoi(argv[i])) != 0) {
+                std::cerr << "ERROR: Bad trigger enable bit specified." << std::endl;
+                return VT_TRIGGER_ENA_BIT_ERR;
+            }
+        } else if (strcmp("--get-trigger-ena-mask", argv[i]) == 0) {
+            u32 ena_mask = vata.get_trigger_ena_mask();
+            std::cout << ena_mask << std::endl;
+        } else if (strcmp("--force-trigger", argv[i]) == 0) { 
+            vata.force_trigger();
+        } else if (strcmp("--set-ack-timeout", argv[i]) == 0) {
+            if (++i >= argc) {
+                std::cerr << "ERROR: No timeout specified." << std::endl;
+                return VT_PARSE_ARGS_ERR;
+            }
+            u32 timeout = (u32)atoi(argv[i]);
+            vata.set_trigger_ack_timeout(timeout);
+        } else if (strcmp("--get-ack-timeout", argv[i]) == 0) {
+            u32 timeout = vata.get_trigger_ack_timeout();
+            std::cout << timeout << std::endl;
+        } else if (strcmp("--get-event-count", argv[i]) == 0) { 
+            std::cout << vata.get_event_count() << std::endl;
+        } else if (strcmp("--reset-event-count", argv[i]) == 0) { 
+            vata.reset_event_count();
+        } else if (strcmp("--get-n-fifo", argv[i]) == 0) { 
+            std::cout << vata.get_n_fifo() << std::endl;
+        } else if (strcmp("--single-read-fifo", argv[i]) == 0) { 
+            if (vata.get_n_fifo() == 0) {
+                std::cout << "FIFO empty." << std::endl;
+            } else { 
+                std::vector<u32> data;
+                int nread;
+                u32 nremain;
+                vata.read_fifo(data, nread, nremain);
+                for (int i=0; i<nread; i++) {
+                    printf("%08X", data[i]);
+                }
+                std::cout << std::endl;
+            }
+        } else if (strcmp("--read-fifo", argv[i]) == 0) { 
+            if (vata.get_n_fifo() == 0) {
+                std::cout << "FIFO empty." << std::endl;
+            } else { 
+                std::vector<u32> data;
+                int nread;
+                u32 nremain;
+                while (vata.get_n_fifo() > 0) {
+                    vata.read_fifo(data, nread, nremain);
+                    for (int i=0; i<nread; i++) {
+                        printf("%08X", data[i]);
+                    }
+                    std::cout << std::endl;
+                }
+            }
+        } else {
+            std::cerr << "ERROR: Unrecognized command line option: " << argv[i] << std::endl;
+            return VT_PARSE_ARGS_ERR;
+        }
+    }
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2) {
+        usage(argv[0]);
+        return VT_PARSE_ARGS_ERR;
+    }
+
+    VataCtrl vata;
+    try {
+        vata = get_vata_from_args(argc, argv);
+    } catch (int e) {
+        usage(argv[0]);
+        return VT_VATA_NUM_ERR;
+    }
+
+    int ret;
+    if ((ret = parse_args(vata, argc-2, argv+2)) == VT_PARSE_ARGS_ERR) {
+        usage(argv[0]);
+    }
+    return ret;
+}
+// vim: set ts=4 sw=4 sts=4 et:
---
+++ src/petalinux/apps/silayer-lib/src/dac_ctrl.cpp
@@ -0,0 +1,142 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <cstring>
+#include "dac_ctrl.hpp"
+
+int parse_silayer_side(char *silayer_side_str, enum SilayerSide *silayer_side) {
+    if (strncmp("A", silayer_side_str, 1) == 0) {
+        *silayer_side = SideA;
+    } else if (strncmp("B", silayer_side_str, 1) == 0) {
+        *silayer_side = SideB;
+    } else {
+        return 1;
+    }
+    return 0;
+}
+
+int parse_dac_choice(char *dac_choice_str, enum DacChoice *dac_choice) {
+    if (strncmp("cal", dac_choice_str, 3) == 0) {
+        *dac_choice = CalDac;
+    } else if (strncmp("vth", dac_choice_str, 3) == 0) {
+        *dac_choice = VthDac;
+    } else {
+        return 1;
+    }
+    return 0;
+}
+
+int parse_set_counts_args(char *silayer_side_str, char *dac_choice_str, char *counts_str,
+        SilayerSide *silayer_side, DacChoice *dac_choice, u32 *counts) {
+    if (parse_silayer_side(silayer_side_str, silayer_side) != 0) {
+        return 1;
+    }
+    if (parse_dac_choice(dac_choice_str, dac_choice) != 0) {
+        return 1;
+    }
+    if (strncmp("cal", dac_choice_str, 3) == 0) {
+        *dac_choice = CalDac;
+    } else if (strncmp("vth", silayer_side_str, 3) == 0) {
+        *dac_choice = VthDac;
+    } else {
+        return 1;
+    }
+    *counts = (u32)atoi(counts_str); 
+    return 0; // success
+}
+
+// There should be only a single calibrator.
+DacCtrl::DacCtrl() {
+    axi_baseaddr = DAC_AXI_BASEADDR;
+    axi_highaddr = DAC_AXI_HIGHADDR;
+    paxi = NULL;
+}
+
+// Destructor performs un-mmapping.
+DacCtrl::~DacCtrl() {
+    // Unmap axi...
+    if (paxi != NULL)
+        if (unmmap_axi() != 0)
+            std::cerr << "ERROR: an unmmap_addr call failed in destructor." << std::endl;
+}
+
+int DacCtrl::mmap_axi() {
+    if ( (axi_fd = open("/dev/mem", O_RDWR | O_SYNC)) == -1) {
+        std::cerr << "ERROR: could not open /dev/mem." << std::endl;
+        throw "ERROR: could not mmap axi.";
+    }
+    u32 span = axi_highaddr - axi_baseaddr + 1;
+    void *vbase = mmap(NULL, span, PROT_READ | PROT_WRITE, MAP_SHARED, axi_fd, axi_baseaddr);
+    if (vbase == MAP_FAILED) {
+        throw "ERROR: could not mmap axi.";
+    }
+    paxi = (u32 *)vbase;
+    return 0;
+}
+
+int DacCtrl::unmmap_axi() {
+    u32 span = axi_highaddr - axi_baseaddr + 1;
+    return munmap((void *)paxi, span);
+}
+
+// Set the clock speed through the delay register
+// Return 1 if the requested value is too large.
+// 0 otherwise
+int DacCtrl::set_delay(u32 delay) {
+    if (MAX_DELAY_VAL < delay)
+        return 1;
+    if (paxi == NULL)
+        mmap_axi();
+    paxi[DAC_DELAY_REGOFF] = delay;
+    return 0;
+}
+
+// Get the current delay value.
+u32 DacCtrl::get_delay() {
+    if (paxi == NULL)
+        mmap_axi();
+    return paxi[DAC_DELAY_REGOFF];
+}
+
+// Set the dac's input value
+// Return 1 if you try and set a crazy value.
+// Return 0 otherwise.
+int DacCtrl::set_counts(enum SilayerSide silayer_side, enum DacChoice dac_choice, u32 counts) {
+    if (paxi == NULL)
+        this->mmap_axi();
+    //std::cout << "Setting side " << side << " " << whichdac
+    //          << " dac to " << counts << " of 4095" << std::endl;
+    if (MAX_INPUT_VAL < counts) {
+        return 1;
+    }
+
+    if (silayer_side == SideA && dac_choice == CalDac) {
+        paxi[DAC_SELECT_REGOFF] = 1 << SIDEA_CALDAC_SHIFT; //0b0001
+    } else if (silayer_side == SideA && dac_choice == VthDac) { 
+        paxi[DAC_SELECT_REGOFF] = 1 << SIDEA_VTH_SHIFT;    //0b0010
+    } else if (silayer_side == SideB && dac_choice == CalDac) {
+        paxi[DAC_SELECT_REGOFF] = 1 << SIDEB_CALDAC_SHIFT; //0b0100
+    } else if (silayer_side == SideB && dac_choice == VthDac) {
+        paxi[DAC_SELECT_REGOFF] = 1 << SIDEB_VTH_SHIFT;    //0b1000
+    } else {
+        // This should never happen now? I think.
+        std::cerr << "ERROR: How did you even provide bad options????" << std::endl;
+        throw "ERROR: Bad Options, nothing done.";
+    }
+
+    paxi[DAC_INPUT_REGOFF] = counts;
+    paxi[DAC_WRITE_REGOFF] = 0;
+    paxi[DAC_WRITE_REGOFF] = 1;
+    paxi[DAC_WRITE_REGOFF] = 0;
+    return 0;
+}
+
+// Get the current dac input, as stated
+// at the axi register.
+u32 DacCtrl::get_input() {
+    if (paxi == NULL)
+        mmap_axi();
+    return paxi[DAC_INPUT_REGOFF];
+}
+
+// vim: set ts=4 sw=4 sts=4 et:
