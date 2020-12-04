5f675895c31e3362f903631cb3416889de293f86
SeanCGriffin <sean.c.griffin@gmail.com>
2020-12-04 16:45:08 -0500

    Adding scripts and things.

---
+++ scripts/ila-scripts/gen_ila2.tcl
@@ -0,0 +1,18 @@
+update_compile_order -fileset sources_1
+startgroup
+create_bd_cell -type ip -vlnv xilinx.com:ip:ila:6.2 ila_2
+endgroup
+set_property -dict [list CONFIG.C_NUM_OF_PROBES {12} CONFIG.C_ENABLE_ILA_AXI_MON {false} CONFIG.C_MONITOR_TYPE {Native}] [get_bd_cells ila_2]
+connect_bd_net [get_bd_pins ila_2/clk] [get_bd_pins processing_system7_0/FCLK_CLK0]
+connect_bd_net [get_bd_pins ila_2/probe0] [get_bd_pins vata_460p3_axi_inter_2/vata_s0]
+connect_bd_net [get_bd_pins ila_2/probe1] [get_bd_pins vata_460p3_axi_inter_2/vata_s1]
+connect_bd_net [get_bd_pins ila_2/probe2] [get_bd_pins vata_460p3_axi_inter_2/vata_s2]
+connect_bd_net [get_bd_pins ila_2/probe3] [get_bd_pins vata_460p3_axi_inter_2/vata_s_latch]
+connect_bd_net [get_bd_pins ila_2/probe4] [get_bd_pins vata_460p3_axi_inter_2/vata_i1]
+connect_bd_net [get_bd_pins ila_2/probe5] [get_bd_pins vata_460p3_axi_inter_2/vata_i3]
+connect_bd_net [get_bd_pins ila_2/probe6] [get_bd_pins vata_460p3_axi_inter_2/vata_i4]
+connect_bd_net [get_bd_ports DIG_ASIC_2_OUT_5_IN] [get_bd_pins ila_2/probe7]
+connect_bd_net [get_bd_ports DIG_ASIC_2_OUT_6_V_IN] [get_bd_pins ila_2/probe8]
+connect_bd_net [get_bd_ports TRIG_HIT_IN] [get_bd_pins ila_2/probe9]
+connect_bd_net [get_bd_pins ila_2/probe10] [get_bd_pins sync_vata_distn_0/vata_hits]
+connect_bd_net [get_bd_pins ila_2/probe11] [get_bd_pins sync_vata_distn_0/force_trigger]
---
+++ src/petalinux/users/scripts/init_and_enable.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Enabling mio..."
+./mio_init.sh
+
+echo "Enabling FEE..."
+./fee_enable.sh
