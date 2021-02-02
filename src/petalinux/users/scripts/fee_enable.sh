77f121e3b87534b66971065ec73b441f2098028f
Adam Schoenwald <aschoen1@umbc.edu>
2021-02-02 17:48:15 -0500

    adding delay for fee power up before binding eeprom drivers

--- src/petalinux/users/scripts/fee_enable.sh
+++ src/petalinux/users/scripts/fee_enable.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 echo 1 > /sys/class/gpio/gpio906/value
+echo "Pausing for FEE power up..."
+sleep 1
 echo -n "spi1.2" > /sys/bus/spi/drivers/at25/bind
 echo -n "spi2.2" > /sys/bus/spi/drivers/at25/bind
