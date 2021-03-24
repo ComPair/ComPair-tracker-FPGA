fee37a6683d171ccdf598a3dd5a41b44c18a23ad
Sean Griffin <sean.c.griffin@gmail.com>
2021-03-24 17:54:34 -0400

    Fixing a rootfs build error.

--- src/petalinux/users/copy-scripts.sh
+++ src/petalinux/users/copy-scripts.sh
@@ -16,3 +16,8 @@ scp -r scripts2/ root@${IP_ADDR}:${DEST_DIR}
 scp -r cfg/ root@${IP_ADDR}:${DEST_DIR}/zynq/config/
 scp -r lucas/profile.d/* root@${IP_ADDR}:/etc/profile.d/
 scp -r sean/profile.d/* root@${IP_ADDR}:/etc/profile.d/
+
+
+ssh root@${IP_ADDR} "mkdir -p ${DEST_DIR}/zynq/log/supervisord/"
+ssh root@${IP_ADDR} "ln -fs ${DEST_DIR}/scripts/fee_disable.sh /etc/rc6.d/K999_fee_disable.sh"
+ssh root@${IP_ADDR} "ln -fs ${DEST_DIR}/scripts2/onboot.sh /etc/rcS.d/S999_onboot.sh"
